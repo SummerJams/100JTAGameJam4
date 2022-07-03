9f0f48c5beb6cce95ee2f9252d3d6e523e567cd5
Ihor <igormochalov1881@gmail.com>
2022-07-03 05:57:53 +0300

    trail effect

--- Assets/Scripts/Player/TopDownMovement.cs
+++ Assets/Scripts/Player/TopDownMovement.cs
@@ -12,7 +12,7 @@ public class TopDownMovement : MonoBehaviour
     [SerializeField] private Transform _rightSideOfPlayer;
     [SerializeField] private float _moveSpeed;
     [Header("Dash properties")]
-    [SerializeField] private ParticleSystem _dust;
+    [SerializeField] private TrailRenderer _trailEffect;
     [SerializeField] private float _dashDuration;
     [SerializeField] private Ram _ram;
 
@@ -60,8 +60,6 @@ public class TopDownMovement : MonoBehaviour
         {
             StartCoroutine(Dash());
             _dashTimer = _ram.DashCooldown;
-            ParticleSystem dust = Instantiate(_dust, transform);
-            Destroy(dust.gameObject, 1f);
         }
 
         if (!_facingRight && _movement.x < 0)
@@ -84,9 +82,11 @@ public class TopDownMovement : MonoBehaviour
         _dashSpeed = _moveSpeed * 3;
         _moveSpeed = _dashSpeed;
         _ram.IsDashing = true;
+        _trailEffect.emitting = true;
         yield return new WaitForSeconds(_dashDuration);
         _ram.IsDashing = false;
         _moveSpeed = temp;
+        _trailEffect.emitting = false;
     }
 
     private void Death()
